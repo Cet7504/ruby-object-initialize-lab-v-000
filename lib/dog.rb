12b1c677c153cfe9244b0b11ed474dfbde5a9108
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2020-11-04 03:35:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,8 @@
 class Dog
-  
-  def initialize(name, breed="Mutt")
+
+  def initialize(name, breed= "Mutt")
     @name = name
     @breed = breed
   end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
 
 end
